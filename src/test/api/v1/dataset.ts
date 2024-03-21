c66b7ddbd6783cad031d2d34b6d20cb8c9247a88
HaroldS10 <haroldsanty13@gmail.com>
2024-03-21 01:47:39 -0500

    creation of the price filter operation

--- src/test/api/v1/dataset.ts
+++ src/test/api/v1/dataset.ts
@@ -101,7 +101,7 @@ export const mobiles: Mobile[] = [
         frontCamera: "10MP",
         rearCamera: "12MP (Wide) + 12MP (Ultra Wide) + 12MP (Telephoto)",
         features: ["Foldable display", "S Pen support", "Water resistance"],
-        price: "1799",
+        price: 1799,
         screenType: "AMOLED",
         rearCameraResolution: "12MP + 12MP + 12MP",
         frontCameraResolution: "10MP",
